0fe335fc3cb093768fe926046d1e4d12809c1766
DaviFelipe3340 <davifelipe2862@gmail.com>
2025-05-23 17:25:08 +0000

    aula 23/05/2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,78 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText.jsx";
 
 export default function App() {
+  const texts = [
+    { title: "Meu titulo", text: "meu texto" },
+    { title: "Meu titulo", text: "meu texto" },
+    { title: "Meu titulo", text: "meu texto" },
+    { title: "Meu titulo", text: "meu texto" },
+  ];
   return (
+    //React Fragment
     <>
-      <MyText />
-      <MyText />
-      <MyText />
+    {
+      texts.map((obj, index) => {
+       return <MyText key={index} title={`${obj.title} ${index + 1}`}> 
+          {`${obj.text} ${index + 1}`}
+        </MyText>
+})}
     </>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    //  {
+    //  
+    //  texts.map((obj, index) => (
+    //    <MyText title={obj.title + " " + (index + 1)}>
+    //      {obj.text + " " + (index + 1)}
+    //    </MyText>
+    //  ))
+    //  
+    //  }
+
+
+
   );
 }
